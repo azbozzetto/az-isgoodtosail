f9c346e30fc8d76a8eebaf99d87af7effd47ee75
Alesander Zorroza <azbozzetto@gmail.com>
2025-07-25 23:18:33 -0300

    Fix Spanish month name

--- src/main.py
+++ src/main.py
@@ -47,7 +47,7 @@ CSV_PATH = './res/shn_data'
 
 MONTH_NAMES = {
     '1': 'Enero', '2': 'Febrero', '3': 'Marzo', '4': 'Abril', '5': 'Mayo',
-    '6': 'Junio', '7': 'Julio', '8': 'Agosto', '9': 'Setiembre',
+    '6': 'Junio', '7': 'Julio', '8': 'Agosto', '9': 'Septiembre',
     '10': 'Octubre', '11': 'Noviembre', '12': 'Diciembre'
 }
 
